af10ec4ff6a554a042362b0e8182753f58d95fa5
JoeyZ <76706398+JoeyZeee@users.noreply.github.com>
2023-10-25 07:59:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 from flask import Flask, request, render_template
+from bardapi import Bard
 import os
 
 app = Flask(__name__)
 
-# Define the directory where uploaded GPX files will be stored
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+token = 'cQiPsFSGiWDV4J_Oh9EPUI03XQRb_Tnnc6n5H_DkfuT3pgCp06FnZ6J6cnWHvp8SxRcKwA.'
+question = "Hi! How are you today?"
 
-# Ensure the upload folder exists
-if not os.path.exists(UPLOAD_FOLDER):
-		os.makedirs(UPLOAD_FOLDER)
+print(Bard(token, timeout=10).get_answer(question)['content'])
 
 @app.route('/')
 def index():
-		return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_gpx():
-		if 'gpx_file' not in request.files:
-				return 'No file part'
+    if 'gpx_file' not in request.files:
+        return 'No file part'
 
-		gpx_file = request.files['gpx_file']
+    #gpx_file = request.files['gpx_file']
 
-		if gpx_file.filename == '':
-				return 'No selected file'
+    if gpx_file.filename == '':
+        return 'No selected file'
 
-		if gpx_file:
-				file_path = os.path.join(app.config['UPLOAD_FOLDER'], gpx_file.filename)
-				gpx_file.save(file_path)
-				return 'GPX file uploaded successfully'
+    if gpx_file:
+        return 'GPX file uploaded successfully'
+
+        return 'File uploaded and saved as coordinates.txt'
 
 if __name__ == '__main__':
-		app.run(debug=True)
+    app.run(debug=True)

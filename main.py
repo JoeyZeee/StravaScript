2831002af7de317c369ec77cfd12bf99eae5dfa0
JoeyZ <76706398+JoeyZeee@users.noreply.github.com>
2023-10-28 10:34:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, render_template
+from bardapi import Bard
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Read the contents of the "BardAnswer.txt" file
+    answer_file_path = os.path.join('responses', 'BardAnswer.txt')
+    with open(answer_file_path, 'r') as answer_file:
+        bard_answer = answer_file.read()
+
+    return render_template('index.html', bard_answer=bard_answer)
 
 @app.route('/upload', methods=['POST'])
 def upload_txt():
@@ -20,16 +26,12 @@ def upload_txt():
     # Define the path where you want to save the file
     file_path = os.path.join('uploads', txt_file.filename)
 
-    # Debug: Print the file path before deleting
-    #print("File path:", file_path)
-
     # Delete any existing files in the "uploads" folder
     existing_files = os.listdir('uploads')
     for existing_file in existing_files:
         existing_file_path = os.path.join('uploads', existing_file)
         try:
             os.remove(existing_file_path)
-            #print("Deleted existing file:", existing_file_path)
         except Exception as e:
             print("Error deleting existing file:", str(e))
 
@@ -41,8 +43,25 @@ def upload_txt():
         with open(file_path, 'wb') as f:
             f.write(file_contents)
 
-        return 'Text file uploaded and saved successfully! Press the arrow to return to the previous page, and see what Bard suggested!'
-    
+        token = 'cgiPsMSxouMUxto8TTqyhgXvrDU8TTZHQnptASvJN3G4XKDgDLDkRNGOyzfaVbBQuvlXvg.'
+        bard = Bard(token=token)
+        answer = bard.get_answer("How you doing?")['content']
+
+        # Delete any existing files in the "responses" folder
+        existing_files = os.listdir('responses')
+        for existing_file in existing_files:
+            existing_file_path = os.path.join('responses', existing_file)
+            try:
+                os.remove(existing_file_path)
+            except Exception as e:
+                print("Error deleting existing file:", str(e))
+
+        # Save the BARD API answer into a text file
+        answer_file_path = os.path.join('responses', 'BardAnswer.txt')
+        with open(answer_file_path, 'w') as answer_file:
+            answer_file.write(answer)
+        
+        return render_template('index.html', bard_answer=answer)  # Fix the indentation here
 
 if __name__ == '__main__':
     app.run(debug=True)

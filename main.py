1aa35a163c8b0ea5547d7d63d78ff2308b2667eb
JoeyZ <76706398+JoeyZeee@users.noreply.github.com>
2023-10-27 19:32:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
 from flask import Flask, request, render_template
-from bardapi import Bard
 import os
 
 app = Flask(__name__)
-
-token = 'cQiPsFSGiWDV4J_Oh9EPUI03XQRb_Tnnc6n5H_DkfuT3pgCp06FnZ6J6cnWHvp8SxRcKwA.'
-question = "Hi! How are you today?"
-
-print(Bard(token, timeout=10).get_answer(question)['content'])
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/upload-page')
+def upload_page():
+    return render_template('upload.html')
+
 @app.route('/upload', methods=['POST'])
-def upload_gpx():
-    if 'gpx_file' not in request.files:
+def upload_txt():
+    txt_file = request.files['txt_file']
+
+    if 'txt_file' not in request.files:
         return 'No file part'
 
-    #gpx_file = request.files['gpx_file']
-
-    if gpx_file.filename == '':
+    if txt_file.filename == '':
         return 'No selected file'
 
-    if gpx_file:
-        return 'GPX file uploaded successfully'
+    # Define the path where you want to save the file
+    file_path = os.path.join('uploads', txt_file.filename)
 
-        return 'File uploaded and saved as coordinates.txt'
+    # Debug: Print the file path before deleting
+    #print("File path:", file_path)
+
+    # Delete any existing files in the "uploads" folder
+    existing_files = os.listdir('uploads')
+    for existing_file in existing_files:
+        existing_file_path = os.path.join('uploads', existing_file)
+        try:
+            os.remove(existing_file_path)
+            #print("Deleted existing file:", existing_file_path)
+        except Exception as e:
+            print("Error deleting existing file:", str(e))
+
+    if txt_file:
+        # Get the contents of the uploaded file
+        file_contents = txt_file.read()
+
+        # Save the new file
+        with open(file_path, 'wb') as f:
+            f.write(file_contents)
+
+        return 'Text file uploaded and saved successfully'
 
 if __name__ == '__main__':
     app.run(debug=True)
